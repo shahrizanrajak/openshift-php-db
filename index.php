2c588063fc008dacbc08496147ff006399e93043
hishamaderis <hisham.aderis@gmail.com>
2020-06-13 12:09:33 +0800

    added $ to variables

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ table, th, td {
 <body>
 <h1>Avengers<h1>
 <?php
-$servername = "MARIADB_SERVICE_HOST";
-$username = "MYSQL_USER";
-$password = "MYSQL_PASSWORD";
-$dbname = "MYSQL_DATABASE";
+$servername = "$MARIADB_SERVICE_HOST";
+$username = "$MYSQL_USER";
+$password = "$MYSQL_PASSWORD";
+$dbname = "$MYSQL_DATABASE";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
